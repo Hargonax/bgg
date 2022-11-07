de00d49ba03141705c1bf880c88f12c968b0f7f4
alejandro.calle <alejandro.calle@finleap.com>
2022-11-07 09:19:16 +0100

    Nuevos usuarios.

--- src/main/java/es/bifacia/bgg/utils/Users.java
+++ src/main/java/es/bifacia/bgg/utils/Users.java
@@ -3,6 +3,7 @@ package es.bifacia.bgg.utils;
 public abstract class Users {
 	public static final String ANMA = "Anma";
 	public static final String DIANA = "didiita";
+	public static final String DUN_DARACH = "jgm72";
 	public static final String GUILLE_SORIA = "nkerensky";
 	public static final String MACKLAU = "macklau";
 	public static final String REUNER = "Reuner";
